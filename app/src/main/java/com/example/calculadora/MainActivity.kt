b03281fdad18998e76d8338bdee1040ed7194dd2
DiegoVR17 <diego.velez1@udea.edu.co>
2023-09-09 12:30:02 -0500

    Second Commit

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.calculadora
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -7,14 +8,15 @@ import com.example.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var num1: Double = 0.0
+    private var operac: Int = 0
+    private var result: Double = 0.0
     private lateinit var mainActivityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         val view = mainActivityMainBinding.root
         setContentView(view)
-
-        var index = 0
 
         mainActivityMainBinding.btn0.setOnClickListener {
             writeOpera("0")
@@ -53,19 +55,31 @@ class MainActivity : AppCompatActivity() {
         }
 
         mainActivityMainBinding.btnAdd.setOnClickListener {
+            makeOpera("+")
+            operac = 1
         }
 
-        mainActivityMainBinding.btnSubtract.setOnClickListener {  }
+        mainActivityMainBinding.btnSubtract.setOnClickListener {
+            makeOpera("-")
+            operac = 2
+        }
+        mainActivityMainBinding.btnMultiply.setOnClickListener {
+            makeOpera("*")
+            operac = 3
+        }
 
-        mainActivityMainBinding.btnMultiply.setOnClickListener {  }
+        mainActivityMainBinding.btnDivide.setOnClickListener {
+            makeOpera("/")
+            operac = 4
+        }
 
-        mainActivityMainBinding.btnDivide.setOnClickListener {  }
+        mainActivityMainBinding.btnClear.setOnClickListener {
+            clear()
+        }
 
-        mainActivityMainBinding.btnClear.setOnClickListener {  }
-
-        mainActivityMainBinding.btnResult.setOnClickListener {  }
-            makeOpera("=", index)
-            index = 0
+        mainActivityMainBinding.btnResult.setOnClickListener {
+            getResult()
+        }
     }
 
     private fun writeOpera(valButton:String){
@@ -77,17 +91,47 @@ class MainActivity : AppCompatActivity() {
         mainActivityMainBinding.editTextOpera.setText(textOpera)
     }
 
-    private fun makeOpera(valButton:String,index: Int){
-        val memoryArray = mutableListOf<Int>()
+    private fun makeOpera(valButton:String){
         val valOpera = mainActivityMainBinding.editTextOpera.text.toString()
         if (valOpera.isEmpty()) {
             Toast.makeText(this,"Debe escribir un número a operar", Toast.LENGTH_SHORT).show()
             return
         }
-        else {memoryArray.add(index,valOpera.toInt())
-             mainActivityMainBinding.editTextOpera.setText("")
+        else{
+            num1 = mainActivityMainBinding.editTextOpera.text.toString().toDouble()
+            val textResult = valOpera+valButton
+            mainActivityMainBinding.editTextResult.setText(textResult)
+            mainActivityMainBinding.editTextOpera.setText("")
         }
+    }
 
+    private fun getResult() {
+        val valOpera = mainActivityMainBinding.editTextOpera.text.toString()
+        val valOpera1 = mainActivityMainBinding.editTextResult.text.toString()
+        if ((valOpera.isEmpty()) || (valOpera1.isEmpty())){
+            Toast.makeText(this,"Debe escribir un número a operar", Toast.LENGTH_SHORT).show()
+            return
+        }
+        else{
+            val expres = valOpera1 + valOpera
+            mainActivityMainBinding.editTextResult.setText(expres)
+            val num2 = valOpera.toDouble()
+            when(operac){
+                1 -> result = num1 + num2
+                2 -> result = num1 - num2
+                3 -> result = num1 * num2
+                4 -> result = num1 / num2
+            }
+            mainActivityMainBinding.editTextOpera.setText(result.toString())
+            mainActivityMainBinding.editTextOpera.setTextColor(Color.parseColor("#3DF73A"))
+        }
+    }
+
+    private fun clear(){
+        mainActivityMainBinding.editTextResult.setText("")
+        mainActivityMainBinding.editTextOpera.setText("")
+        num1 = 0.0
+        mainActivityMainBinding.editTextOpera.setTextColor(Color.parseColor("#111111"))
     }
 
 }
